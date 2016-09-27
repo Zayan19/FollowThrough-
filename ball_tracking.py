3030c3141b87ed12478dd6fd65428d0024f9e4e6
GeorgePlukov <gplukov@gmail.com>
2016-09-27 14:38:22 -0400

    working on tracking the color orange

--- ball_tracking.py
+++ ball_tracking.py
@@ -18,9 +18,11 @@ args = vars(ap.parse_args())
 # list of tracked points
 blueLower = (110,50,50)
 blueUpper = (130,255,255)
-greenLower = (0, 150, 155)
-greenUpper = (248, 255, 255)
-
+# greenLower = (0, 150, 155)
+# greenUpper = (248, 255, 255)
+# orange water bottle cap
+greenLower = (0,86,211)
+greenUpper = (8,255,255)
 # greenLower = (29, 86, 6)
 # greenUpper = (64, 255, 255)
 pts = deque(maxlen=args["buffer"])
@@ -48,14 +50,15 @@ while True:
 
 	# resize the frame, blur it, and convert it to the HSV
 	# color space
-	# frame = imutils.resize(frame, width=1200)
+	frame = imutils.resize(frame, width=1200)
 	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
 	# construct a mask for the color "green", then perform
 	# a series of dilations and erosions to remove any small
 	# blobs left in the mask
 	mask = cv2.inRange(hsv, greenLower, greenUpper)
-	mask = cv2.erode(mask, None, iterations=1)
+	mask = cv2.erode(mask, None, iterations=2)
+	
 	mask = cv2.dilate(mask, None, iterations=2)
 
 	# find contours in the mask and initialize the current
@@ -77,7 +80,7 @@ while True:
 		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
 		# only proceed if the radius meets a minimum size
-		if radius > 10:
+		if radius > 40:
 			# draw the circle and centroid on the frame,
 			# then update the list of tracked points
 			cv2.circle(frame, (int(x), int(y)), int(radius),
