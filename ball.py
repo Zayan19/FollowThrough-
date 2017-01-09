a0ff9a2ba4c0e94841b93051dcc8b997a0ea3e84
philip-h <philip.habib6347@gmail.com>
2017-01-09 14:16:17 -0500

    Branch refactor

--- ball.py
+++ ball.py
@@ -5,12 +5,10 @@ import numpy as np
 import argparse
 import cv2
 import imutils
+import math
 
 
 
-# takes a Direction (tuple list, integer (0-3), int frames)
-# tuple is a list of points that the ball has last travelled
-# 0 = up, 1 = down, 2 = left, 3 = right
 def direction (pts, direction = 0, frames = 3):
 	# The return variable
 	isMovingInDirection = False
@@ -34,41 +32,47 @@ def direction (pts, direction = 0, frames = 3):
 				isMovingInDirection = isMovingInDirection or True
 	return isMovingInDirection
 
+def angle(cx, cy, ex, ey) :
+   dy = ey - cy;
+   dx = ex - cx;
+   global theta
+   theta = math.atan2(dy, dx);
+   # // range (-PI, PI]
+   theta *= (180 / math.pi);
+   theta = abs(theta)
+   # theta=180-theta
+   return theta
 
+theta = 0
+foundAngle = False
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-# ap.add_argument("-v", "--video","--/home/zack/Uni/Capstone/ball-tracking/FT_make.MOV")
+ap.add_argument("-v", "--video", nargs="?", type=str)
 ap.add_argument("-b", "--buffer", type=int, default=15,
 	help="max buffer size")
 args = vars(ap.parse_args())
 
-# define the lower and upper boundaries of the "green"
+# define the lower and upper boundaries of the "orange"
 # ball in the HSV color space, then initialize the
 # list of tracked points
-blueLower = (110,50,50)
-blueUpper = (130,255,255)
-# greenLower = (0, 150, 155)
-# greenUpper = (248, 255, 255)
-# orange water bottle cap
-greenLower = (0,86,211)
-greenUpper = (8,255,255)
 orangeLower = (3,100,160)
 orangeUpper = (10,180,240)
 
-# greenLower = (29, 86, 6)
-# greenUpper = (64, 255, 255)
 pts = []
-# ([17, 15, 100], [50, 56, 200]),
-# if a video path was not supplied, grab the reference
-# to the webcam
-# if not args.get("video", False):
 
-#if True then it runs your native camera, otherwise the specified video
-if False:
-	camera = cv2.VideoCapture(0)
-# otherwise, grab a reference to the video file
+# If a video path is supplied via cl arg, run it
+if args.get("video"):
+	camera = cv2.VideoCapture(args.get("video"))
+# Otherwise, use the web cam
 else:
-	camera = cv2.VideoCapture("test_videos/FT_make.MOV")
+	camera = cv2.VideoCapture(0)
+
+#counter to check how many times the points have gone in the down and left direction
+downLeft = 0
+
+#initialize maxX,maxY values which represent the apex of the ball arc
+maxX=800
+maxY=800
 
 # keep looping
 while True:
@@ -82,7 +86,7 @@ while True:
 
 	# resize the frame, blur it, and convert it to the HSV
 	# color space
-	frame = imutils.resize(frame, width=1200)
+	frame = imutils.resize(frame, width=900)
 	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
 	# construct a mask for the color "green", then perform
@@ -96,9 +100,9 @@ while True:
 	# find contours in the mask and initialize the current
 	# (x, y) center of the ball
 	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-		cv2.CHAIN_APPROX_NONE)[-2]
+		cv2.CHAIN_APPROX_SIMPLE)[-2]
 	center = None
-	first = True
+
 	# only proceed if at least one contour was found
 	if len(cnts) > 0:
 		# find the largest contour in the mask, then use
@@ -106,13 +110,11 @@ while True:
 		# centroid
 		c = max(cnts, key=cv2.contourArea)
 		((x, y), radius) = cv2.minEnclosingCircle(c);
-        #print out the x and y coordinates
-		# print ("This is x",x,"This is y",y)
+		# ((x2, y2), radius) = cv2.minEnclosingCircle(c);
+
 		M = cv2.moments(c)
 		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
-
-			# continue
 		# only proceed if the radius meets a minimum size
 		if radius > 40:
 			# draw the circle and centroid on the frame,
@@ -120,47 +122,61 @@ while True:
 			cv2.circle(frame, (int(x), int(y)), int(radius),
 				(0, 255, 255), 2)
 			cv2.circle(frame, center, 5, (0, 0, 255), -1)
-
 		# update the points queue
 		pts.append(center)
+
+        # Find the max height of the ball
+        if (y<maxY):
+            maxY=y
+            maxX=x
+
+        length = len(pts) -1
+        if length>=5:
+            # Check to make sure it's going down left for a decent amount of time before computing the angle
+            if   direction(pts,1,5) and (direction(pts,2,5)) and not (direction(pts,0,5)) and not (direction(pts,3,5)):
+                downLeft = downLeft+1
+        if (length >=5):
+            if downLeft >=5 and direction(pts,1,5) and (direction(pts,2,5)) and not (direction(pts,0,5)) and not (direction(pts,3,5)):
+                angle(x,y,maxX,maxY)
+                foundAngle = True
+
 	# Determine direction of ball
 	# Confirm there are at least 10 points before trackign starts
-	length = len(pts) - 1
-	if (length >= 10):
-		# Is it moving up?
-		if(direction(pts, 0, 5)):
-			cv2.putText(frame,"U", (50,50), cv2.FONT_HERSHEY_SIMPLEX, 2, 0)
-		if(direction(pts, 1, 5)):
-			cv2.putText(frame,"D", (100,50), cv2.FONT_HERSHEY_SIMPLEX, 2, 0)
-		if(direction(pts, 2, 5)):
-			cv2.putText(frame,"L", (150,50), cv2.FONT_HERSHEY_SIMPLEX, 2, 0)
-		if(direction(pts, 3, 5)):
-			cv2.putText(frame,"R", (200,50), cv2.FONT_HERSHEY_SIMPLEX, 2, 0)
+
+	# length = len(pts) - 1
+	# if (length >= 10):
+	# 	if(direction(pts, 0, 5)):
+	# 		cv2.putText(frame,"U", (50,50), cv2.FONT_HERSHEY_SIMPLEX, 2, 0)
+	# 	if(direction(pts, 1, 5)):
+	# 		cv2.putText(frame,"D", (100,50), cv2.FONT_HERSHEY_SIMPLEX, 2, 0)
+	# 	if(direction(pts, 2, 5)):
+	# 		cv2.putText(frame,"L", (150,50), cv2.FONT_HERSHEY_SIMPLEX, 2, 0)
+	# 	if(direction(pts, 3, 5)):
+	# 		cv2.putText(frame,"R", (200,50), cv2.FONT_HERSHEY_SIMPLEX, 2, 0)
+
+    #Once the angle is found, print it on screen
+	if (foundAngle):
+		cv2.putText(frame,"Your angle is "+str(int(theta))+" degrees!",(50,70),cv2.FONT_HERSHEY_SIMPLEX,2,(255,0,0),4,0)
+
 	# loop over the set of tracked points
 	for i in xrange(1, len(pts)):
-		# check to see if the center of the ball is in a similar position to last time
-		# I would like it to only poll a certain area but I have to look more
-		# into masking first
-		# if (center != None):
-		# 	if (first or abs(center[0] - pts[len(pts) - 1][0]) > 0):
-		# 		# print(abs(center[0] - pts[0][len(pts) - 1]))
-		# 		first = False
-		# 		# break
 		# if either of the tracked points are None, ignore
 		# them
 		if pts[i - 1] is None or pts[i] is None:
 			continue
 		# otherwise, compute the thickness of the line and
 		# draw the connecting lines
-
-		thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+		# thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
 		cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), 2)
 
-
 	# show the frame to our screen
+	cv2.moveWindow("Frame", 0, 0)
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
-	
+	if key == ord('p'):
+		while True:
+			if cv2.waitKey(1) == ord('p'):
+				break
 	# if the 'q' key is pressed, stop the loop
 	if key == ord("q"):
 		break
