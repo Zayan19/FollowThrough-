7b693e81108e76c2f7ca8c338a95fd312619c7d2
Philip Habib <philip.habib6347@gmail.com>
2017-02-13 19:41:51 -0500

    Working api, working databases, login and register

---
+++ FollowThroughWeb/app/Http/Controllers/ShotsController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+use App\Shots;
+use App\User;
+use Auth;
+
+class ShotsController extends Controller
+{
+    /**
+     * Display all the shots of the current user.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        if (Auth::check()) {
+            //TODO: return a view 
+            $user_id = Auth::user()->id;
+            $shots = User::find($user_id)->shots;
+
+            return response(array(
+                'shots' => $shots->toArray()
+            ), 200);
+        } else {
+            //TODO: return a view styll
+            return response(array('error'=> $user_id),200);
+        }
+    }
+
+    /**
+     * Api to get the shot information of a requested user
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index_api($id)
+    {
+        $user = User::find($id);
+        if (is_null($user)) {
+            return response(array('error'=>'user not found'));
+        }
+        $shots = $user->shots;
+        return response(array('shots' => $shots));
+
+
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Shots::create($request->all());
+        return response(array('message'=>'Shot created successfully'));
+    }
+
+    /**
+     * Display data about a specific shot!
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- FollowThroughWeb/routes/api.php
+++ FollowThroughWeb/routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:api');
+});
+
+Route::post('/shot', 'ShotsController@store');
+Route::get('/shots/{id}', 'ShotsController@index_api');
+Route::post('/user', 'UserController@user');
