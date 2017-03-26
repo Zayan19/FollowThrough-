7b631f5c6df047a773237c7e14b756484fbed97e
Philip Habib <philip.habib6347@gmail.com>
2017-03-26 16:48:19 -0400

    Modularized Networking and Ball module for testing

---
+++ ShotRecognition/Networking.py
@@ -0,0 +1,73 @@
+from datetime import datetime
+import urllib, urllib2
+import Ball_Tracking
+
+class Networking_Python:
+    """ Wrapper for posting data (From python module) """
+
+    def __init__(self, entry_angle, exit_angle, arc_height, zone = 0):
+        self.entry_angle = entry_angle
+        self.exit_angle = exit_angle
+        self.arc_height = arc_heigth
+        self.zone = zone
+
+        self.over_under = 'over' if arc_height > 4 else 'under'
+        self.zone = None
+        self.user_id = None
+
+    def post(self, url):
+        """ Post data given by constructor to api """
+        data_to_post = {}
+
+        assert self.user_id is not None, "Must login before posting to server"
+        assert self.zone is not None, "Must set zone before posting to server"
+
+        data_to_post['user_id'] = self.user_id 
+        data_to_post['zone'] = self.zone
+        data_to_post['over_under_shot'] = self.over_under
+        data_to_post['exit_angle'] = self.exit_angle
+        data_to_post['entry_angle'] = self.entry_angle
+        data_to_post['arc_height'] = self.arc_height
+        data_to_post['made'] = self.made
+        data_to_post['time_of_shot'] = _format_datetime(datetime.now())
+
+        url_values = urllib.urlencode(data_to_post)
+
+        page = None
+        try:
+           page = urllib2.urlopen(url, url_values)
+        except urllib2.HTTPError as e:
+            print e.read()
+
+        return page.read()
+
+    def _format_datetime(date):
+        str_date = str(date)
+        i = str_date.index('.')
+        return str_date[:i]
+
+
+class Networking_Hardware:
+    """ Wrapper for posting data (From arduino module) """
+
+    def __init__(self, time):
+        self.time = time
+
+    def post(self, url):
+        """ Post data given by constructor to api """
+        data_to_post = {}
+
+        assert self.user_id is not None, "Must login before posting to server"
+
+        data_to_post['user_id'] = self.user_id 
+        data_to_post['time_of_shot'] = self.time 
+
+        url_values = urllib.urlencode(data_to_post)
+
+        page = None
+        try:
+           page = urllib2.urlopen(url, url_values)
+        except urllib2.HTTPError as e:
+            print e.read()
+
+        return page.read()
