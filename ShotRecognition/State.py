571fe0304b992f1c1b129b6111b32f4a92905804
GeorgePlukov <gplukov@gmail.com>
2017-03-27 18:38:45 -0400

    Added data structure for designing a menu page

---
+++ ShotRecognition/State.py
@@ -0,0 +1,29 @@
+from PyQt4 import QtGui, QtCore
+
+class State:
+    def __init__(self, state=0, layout=""):
+        self.state = state
+        self.widgets = []
+        self.num_of_widgets = 0
+        # Create the layout object
+        if layout == "vbox":
+            self.layout = QtGui.QVBoxLayout(self)
+
+    def get_layout (self):
+        return self.layout
+
+    def add_widget(self, widget):
+        # add the widget to the layout and add it to the ist of widgets
+        self.layout.addWidget(widget)
+        self.widgets.push(widget)
+
+    def remove_all_widgets(self):
+        for i in self.widgets:
+            self.layout.remove_widget()
+        self.widgets = []
+
+    def remove_widget(self, widget):
+        self.layout.removeWidget(widget)
+
+    def get_widgets(self):
+        return self.widgets
